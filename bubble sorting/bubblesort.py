187f68b8207d4a49f05d8f5b821b3587b96a774b
X-sudo-c <x1dragonn@gmail.com>
2025-05-26 21:32:14 +0000

    trying to understand bubble sort implementation before i continue

--- bubble sorting/bubblesort.py
+++ bubble sorting/bubblesort.py
@@ -1,14 +1,32 @@
+from typing import List
 
 nums = [5, 3, 8, 6, 2]
 
 
 
-def sort(nums):
-    pass
+print(range(len(nums)))
+
+
+
+x = range(5)
+
+for i in x:
+    print(i)
+
+def sort(nums:List[int])-> List[int]:
+    for i in range(len(nums)-1, 0, -1):
+        for j in range(i):
+            if nums[j] > nums[j + 1]:
+                temp = nums[j]
+        pass
 
 
 
 
-sort(nums)
 
-print(nums)
+
+
+
+#sort(nums)
+
+#print(nums)
