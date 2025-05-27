f3526882425976b77deabc838ea88c057d20d2ea
X-sudo-c <x1dragonn@gmail.com>
2025-05-27 20:52:31 +0000

    bubble

--- bubble sorting/bubblesort.py
+++ bubble sorting/bubblesort.py
@@ -4,21 +4,13 @@ nums = [5, 3, 8, 6, 2]
 
 
 
-print(range(len(nums)))
 
 
-
-x = range(5)
-
-for i in x:
-    print(i)
-
-def sort(nums:List[int])-> List[int]:
-    for i in range(len(nums)-1, 0, -1):
-        for j in range(i):
-            if nums[j] > nums[j + 1]:
-                temp = nums[j]
+#bubble sort algorithm
+def sort(nums):
+    for i in range(n):
         pass
+
 
 
 
