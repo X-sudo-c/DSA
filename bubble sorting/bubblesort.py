a2c5a00998f3647e97e23c0f215d9efd4c7afd16
X-sudo-c <x1dragonn@gmail.com>
2025-05-28 23:48:20 +0000

    bub sort

--- bubble sorting/bubblesort.py
+++ bubble sorting/bubblesort.py
@@ -3,18 +3,27 @@ from typing import List
 nums = [5, 3, 8, 6, 2]
 
 
-
+n = len(nums)
 
 
 #bubble sort algorithm
 def sort(nums):
     for i in range(n):
-        pass
+        for j in range (0, n - i -1):
+            if nums[j] > nums[j + 1]:
+                #Swap elements
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
 
+        return [str(num)for num in nums]
+    
 
+#Run and print result
 
+sorted_nums = sort(nums)
 
-
+print("Sorted (as strings):", sorted_nums)
+    
+    
 
 
 
