3b8955130ea81a9313f9b658ce54b66ac6a3598d
X-sudo-c <x1dragonn@gmail.com>
2025-06-09 15:23:13 +0000

    basic code practice

--- bubble sorting/1/1.c
+++ bubble sorting/1/1.c
@@ -13,20 +13,39 @@ int main(){
     printf("Welcome to simple calculator\n");
     printf("\n");
 
-    printf("Do you want to add subtract of divide? :\n");
+    printf("Do you want to add subtract or divide? :\n");
 
-    scanf("%s", &choice);
+    scanf("%s", choice);
 
 
-    switch (choice){ 
-        case 'add' :
-        printf("enter your first number: \n");
-        scanf("%d", &x);
-        printf("enter your second number: \n");
-        scanf("%d", &y);
-        printf("the sum is %d", x + y);
+    // switch (choice){ 
+    //     case 'add' :
+    //     printf("enter your first number: \n");
+    //     scanf("%d", &x);
+    //     printf("enter your second number: \n");
+    //     scanf("%d", &y);
+    //     printf("the sum is %d", x + y);
 
         
+    // }
+    // so i found out that you cant compare strings in using the switch statement its only used for ints and char
+    //and also you dont need to add & to an array just do scanf("%s", <array name no '&'>)
+
+
+
+    if (strcmp(choice, "add")== 0)
+    {
+        printf("you want to add ?");
+    }
+    else if(strcmp(choice, "subtract")== 0)
+    {
+        printf("you want to subtract");
+    }
+    else if(strcmp(choice, "divide") == 0)
+    {
+        printf("you want to divide");
+    }else{
+        printf("Invalid operation selected");
     }
 
 
---
+++ bubble sorting/1/1.go
@@ -0,0 +1,14 @@
+package main
+
+import ("fmt")
+
+
+
+func main(){
+	fmt.Println("Welcome to simple calculator\n")
+	fmt.Println("\n")
+	var choice int
+
+	fmt.Scanln
+
+}
