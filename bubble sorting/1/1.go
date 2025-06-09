fa804e368cabf717b47acddb8404ce65e61d2e22
X-sudo-c <x1dragonn@gmail.com>
2025-06-09 15:36:42 +0000

    basic code practice

--- bubble sorting/1/1.go
+++ bubble sorting/1/1.go
@@ -6,9 +6,24 @@ import ("fmt")
 
 func main(){
 	fmt.Println("Welcome to simple calculator\n")
-	fmt.Println("\n")
-	var choice int
+	var choice string
+	fmt.Printf("Do you want to add, substract, or divide: ")
+	fmt.Scanln(&choice)
 
-	fmt.Scanln
+	if choice == "add"{
+
+		fmt.Println("You chose add")
+
+	}else if choice == "subtract"{
+
+		fmt.Println("You chose subtract")
+
+	}else if choice == "divide"{
+
+		println("You chose divide")
+
+	}else{
+		fmt.Println("undefined option")
+	}
 
 }
